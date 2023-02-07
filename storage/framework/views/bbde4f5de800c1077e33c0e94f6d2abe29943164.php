0664c9b63263e31011913d7e13132f14799fff0f
Kat-25 <99860924+Kat-25@users.noreply.github.com>
2023-02-07 20:01:34 +0800

    Add files via upload

---
+++ storage/framework/views/bbde4f5de800c1077e33c0e94f6d2abe29943164.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MANAGE COMPLAINTS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+    
+</body>
+</html><?php /**PATH C:\Users\Katrina\example-app\resources\views/products/layout.blade.php ENDPATH**/ ?>
